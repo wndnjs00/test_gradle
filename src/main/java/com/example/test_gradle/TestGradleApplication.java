8be09fffe0f659e30e3290b913c3d91cf2842e7b
wndnjs00 <juwonjeon852@gmail.com>
2024-11-17 18:14:15 +0900

    initial commit

---
+++ src/main/java/com/example/test_gradle/TestGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_gradle/TestGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
